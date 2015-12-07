c965eb1d603feea409a79e93b45ec78f094c6cdd
JimboPower <edoardo.decal@gmail.com>
2015-12-07 01:02:40 +0100

    Added Menu and Earth move

--- Edoardo DC/FirstViewController.swift
+++ Edoardo DC/FirstViewController.swift
@@ -9,53 +9,197 @@
 import UIKit
 
 class ViewController: UIViewController {
-   
-    @IBOutlet weak var constraintTitle: NSLayoutConstraint!
-
     
-    @IBOutlet weak var constraintInizia: NSLayoutConstraint!
+ 
+    @IBAction func ruotaMondo(sender: AnyObject) {
+        let terra2 = UIImageView()
+        terra2.image = UIImage(named: "Terra")
+        terra2.frame = CGRect(x: 71, y: 500, width: 250, height: 250)
+        self.view.addSubview(terra2)
+        let fullRotation = CGFloat(M_PI * 2)
+        let duration = 2.0
+        let delay = 0.0
+        let options = UIViewKeyframeAnimationOptions.CalculationModeLinear
+        
+        UIView.animateKeyframesWithDuration(duration, delay: delay, options: options, animations: {
+            UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 1/3, animations: {
+                
+                terra2.transform = CGAffineTransformMakeRotation(1/3 * fullRotation)
+            })
+            UIView.addKeyframeWithRelativeStartTime(1/3, relativeDuration: 1/3, animations: {
+                terra2.transform = CGAffineTransformMakeRotation(2/3 * fullRotation)
+            })
+            UIView.addKeyframeWithRelativeStartTime(2/3, relativeDuration: 1/3, animations: {
+                terra2.transform = CGAffineTransformMakeRotation(3/3 * fullRotation)
+            })
+            
+            }, completion: {finished in
+        })
+
+    }
+    
+    @IBAction func buttonAvanti1(sender: AnyObject) {
+        
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(3, animations: {
+            self.constraintTopTitle2.constant = -1000
+            self.view.layoutIfNeeded()
+            self.view.layoutIfNeeded()
+        })
+        UIView.animateWithDuration(1, animations: {
+            self.constraintSubTitle2.constant = -1000
+            self.view.layoutIfNeeded()
+        })
+  
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(3, animations: {
+            self.constraintLeadingTextView.constant = -1000
+            self.view.layoutIfNeeded()
+        })
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(3, animations: {
+            self.constraintLeadingAvanti.constant = -1000
+            self.view.layoutIfNeeded()
+        })
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(3, animations: {
+            self.constrainsTopTitle2.constant = 81
+            self.view.layoutIfNeeded()
+        })
+        
+        
+    }
+    @IBAction func buttonInizia(sender: AnyObject) {
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(4, animations: {
+            self.constraintTopTitle.constant = -1000
+            self.view.layoutIfNeeded()
+        })
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(2, animations: {
+            self.constraintTopSubTitle.constant = -500
+            self.view.layoutIfNeeded()
+        })
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(3, animations: {
+            self.constraintLeadingInizia.constant = -1000
+            self.view.layoutIfNeeded()
+        })
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(4, animations: {
+            self.constraintLeadingLuna.constant = -1000
+            self.view.layoutIfNeeded()
+        })
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(3, animations: {
+            self.constraintTopTerra.constant = -1000
+            self.view.layoutIfNeeded()
+        })
+        
+        //////valore constraint2//
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(3, animations: {
+            self.constraintTopTitle2.constant = 81
+            self.view.layoutIfNeeded()
+            self.view.layoutIfNeeded()
+            })
+            UIView.animateWithDuration(1, animations: {
+                self.constraintSubTitle2.constant = 260
+                self.view.layoutIfNeeded()
+            })
+            self.view.layoutIfNeeded()
+            UIView.animateWithDuration(3, animations: {
+                self.constraintTralingTerra2.constant = 230
+                self.view.layoutIfNeeded()
+            })
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(3, animations: {
+            self.constraintLeadingTextView.constant = 15
+            self.view.layoutIfNeeded()
+        })
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(3, animations: {
+            self.constraintLeadingAvanti.constant = 300
+            self.view.layoutIfNeeded()
+        })
+
+        
+        
+
+
+    }
+    
+    /////declare constraint1/////////////////////////
+    @IBOutlet weak var constraintTopTitle2: NSLayoutConstraint!
     
     @IBOutlet weak var constraintLeadingInizia: NSLayoutConstraint!
     
+    @IBOutlet weak var constraintTopTitle: NSLayoutConstraint!
+    
+    @IBOutlet weak var constraintTopSubTitle: NSLayoutConstraint!
+    
+    @IBOutlet weak var constraintLeadingLuna: NSLayoutConstraint!
+   
+    @IBOutlet weak var constraintSubTitle2: NSLayoutConstraint!
+    
+    
+    @IBOutlet weak var constraintTralingTerra2: NSLayoutConstraint!
+    
+        @IBOutlet weak var constraintTopTerra: NSLayoutConstraint!
+    
+    @IBOutlet weak var constrainsTopTitle2: NSLayoutConstraint!
 
+    @IBOutlet weak var constraintLeadingAvanti: NSLayoutConstraint!
+    @IBOutlet weak var constraintLeadingTextView: NSLayoutConstraint!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ///////title///////
-        constraintTitle.constant = 1000
-        
-        //////inizio//////
-        constraintInizia.constant = -1000
-        constraintLeadingInizia.constant = 300
-        
-        ///////azione!//////
-        self.view.layoutIfNeeded()
-        UIView.animateWithDuration(1, animations: {
-            self.constraintTitle.constant = 81
-            self.view.layoutIfNeeded()
-        })
-        self.view.layoutIfNeeded()
-        UIView.animateWithDuration(1, animations: {
-            self.constraintInizia.constant = 670
-            self.view.layoutIfNeeded()
-        })
-        
-        
-        
-        
+        //////where are now?1/////////////////////////
+        constraintLeadingInizia.constant = -1000
+        constraintTopTitle.constant = -1000
+        constraintTopSubTitle.constant = -1000
+        constraintLeadingLuna.constant = -1000
+        constraintTopTerra.constant = -1000
+        constraintTopTitle2.constant = -1000
+        constraintSubTitle2.constant = -1000
+        constraintTralingTerra2.constant = -1000
+      
+        constraintLeadingTextView.constant = -1000
+        constraintLeadingAvanti.constant = -1000
+        constrainsTopTitle2.constant = -1000
        
         
+        //////where are a moment later?//////////////
+       
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(1, animations: {
+            self.constraintTopTitle.constant = 81
+            self.view.layoutIfNeeded()
+        })
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(2, animations: {
+            self.constraintTopSubTitle.constant = 190
+            self.view.layoutIfNeeded()
+        })
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(3, animations: {
+            self.constraintLeadingInizia.constant = 207
+            self.view.layoutIfNeeded()
+        })
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(4, animations: {
+            self.constraintLeadingLuna.constant = 89
+            self.view.layoutIfNeeded()
+        })
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(4, animations: {
+            self.constraintTopTerra.constant = 556
+            self.view.layoutIfNeeded()
+        })
         
-        
-        
-        
-        
-        
-        
-    }
-    
-
   
 }
 
+}
